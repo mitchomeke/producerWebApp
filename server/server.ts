6ce86142590e26b4a85cf7954e84ac5043d04a7a
Mitchell Chukwueku Omeke <164729363+mitchomeke@users.noreply.github.com>
2026-09-17 11:39:00 +0100

    changing how it server.ts

--- server/server.ts
+++ server/server.ts
@@ -605,8 +605,11 @@ app.get('/api/verify-session/songs', async (req, res) => {
     }
 });
 
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Backend running on http://localhost:${PORT}`);
+})
 
-    app.listen(PORT, '0.0.0.0', () => {
-        console.log(`Node.js server listening on port ${PORT}`);
+    app.listen(5000, '0.0.0.0', () => {
+        console.log('Node.js server listening on port 5000');
     });
